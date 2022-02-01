e6a29c10c45c59c13bf3bc71f6cc6d819b47282a
Reddy Tintaya <aaabeeelooon@gmail.com>
2022-02-01 13:47:39 -0400

    refactor url lenght in model

--- apps/bookmarks/models.py
+++ apps/bookmarks/models.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django.utils import timezone
 
 
 class Bookmark(models.Model):
     is_private = models.BooleanField(default=True)
     created_by = models.ForeignKey(User, related_name="bookmarks", on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    url = models.CharField(max_length=100)
-    created_at = models.DateTimeField(default=timezone.now())
 
+    # https://www.geeksforgeeks.org/maximum-length-of-a-url-in-different-browsers/#:~:text=Google%20Chrome%20allows%20the%20maximum,size%202MB(2048%20characters).&text=In%20Firefox%20the%20length%20of,no%20longer%20displays%20the%20URL.
+    url = models.CharField(max_length=2048)
+    created_at = models.DateTimeField()
